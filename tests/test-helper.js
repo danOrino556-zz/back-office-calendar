fef59fb4a6e5c3127a5fd16a3044a050e0f0e5f8
Daniel Robertson <daniel.l.robertson556@gmail.com>
2021-08-30 16:13:17 -0500

    Ember Upgrade 3.1 -> 3.24
    - updated the ember version via ember-cli-update

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'back-office-calendar/app';
+import config from 'back-office-calendar/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
